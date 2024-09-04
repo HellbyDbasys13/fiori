01a50fe93679929c067d3a57ffdcef0531e5be0a
Alexis Ledesma <alexis.ledesma@dbasys13.com>
2024-09-04 23:05:50 +0000

    Se logran ver la card pero no la informacion

--- webapp/controller/fragments/Home.controller.js
+++ webapp/controller/fragments/Home.controller.js
@@ -9,15 +9,7 @@ sap.ui.define([
   return Controller.extend("siar.controller.fragments.Home", {
 
     onInit: function () {
-            // Cargar el archivo JSON desde la carpeta model
-            var oModel = new JSONModel();
-            oModel.loadData(sap.ui.require.toUrl("fiori/model/data.json"));
-            // Asegúrate de que this.getView() no es undefined
-            if (this.getView()) {
-                this.getView().setModel(oModel);
-            } else {
-                console.error("View is not available.");
-            }
+        
     }
 
   });
